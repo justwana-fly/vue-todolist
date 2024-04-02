7551efff07b9893e8d2ec3143c6352e9ce9937e9
justwana-fly <nicolaibarbarasa@gmail.com>
2024-04-02 16:17:11 +0200

    aggiunto metodo per aggiungere e rimuovere oggetti

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
- const todo = [
-     {
-       id: 1,
-       text: `Scarponi`,
-       done: false,
-     },
-     {
-       id: 2,
-       text: `Giacca a vento`,
-       done: false,
-     },
-     {
-       id: 3,
-       text: `Zaino`,
-       done: false,
-     },
-     {
-       id: 4,
-       text: `Cappello`,
-       done: false,
-     },
-     {
-       id: 5,
-       text: `Piccone`,
-       done: false,
-     },
-     {
-       id: 6,
-       text: `Corda`,
-       done: false,
-     },
-     {
-       id: 7,
-       text: `Mappa`,
-       done: false,
-     },
-     {
-       id: 8,
-       text: `Tenda`,
-       done: false,
-     },
-     {
-       id: 9,
-       text: `Sacco a pelo`,
-       done: false,
-     },
-     {
-       id: 10,
-       text: `Bussola`,
-       done: false,
-     },
-   ];
-  
-
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            message: `1`
+            todo: [
+                {
+                    id: 1,
+                    text: `Scarponi`,
+                    done: false,
+                },
+                {
+                    id: 2,
+                    text: `Giacca a vento`,
+                    done: false,
+                },
+                {
+                    id: 3,
+                    text: `Zaino`,
+                    done: false,
+                },
+                {
+                    id: 4,
+                    text: `Cappello`,
+                    done: false,
+                },
+                {
+                    id: 5,
+                    text: `Piccone`,
+                    done: false,
+                },
+                {
+                    id: 6,
+                    text: `Corda`,
+                    done: false,
+                },
+                {
+                    id: 7,
+                    text: `Mappa`,
+                    done: false,
+                },
+                {
+                    id: 8,
+                    text: `Tenda`,
+                    done: false,
+                },
+                {
+                    id: 9,
+                    text: `Sacco a pelo`,
+                    done: false,
+                },
+                {
+                    id: 10,
+                    text: `Bussola`,
+                    done: false,
+                },
+            ]
         };
+    },
+    methods: {
+        addItem() {
+            const newItem = {
+                id: this.todo.length + 1,
+                text: "",
+                done: false
+            };
+            this.todo.push(newItem);
+        },
+        removeItem() {
+            this.todo.pop();
+        }
     }
 }).mount('#app');
-
